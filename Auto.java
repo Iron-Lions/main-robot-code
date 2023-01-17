3496cc8a6d665279602b7bfadb7a53d1b9cd4cb2
Iron Lions <IronLions@proton.me>
2023-01-17 14:57:14 -0500

    Detect color

--- Auto.java
+++ Auto.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -8,18 +11,26 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+enum RGBColor {
+    RED, GREEN, BLUE, INVALID
+}
+
 @Autonomous(name = "Auto", group = "")
 
 public class Auto extends LinearOpMode {
 
     private static final double RUN_TIME = 25;
     private static final double MOVE_SPEED = 0.5;
+    private static final double DIST_TO_TIME = 3000 / 31.75;
+    private static final double SERVO_LOWER = 0.509;
     private ElapsedTime runtime = new ElapsedTime();
 
     private DcMotor FL;
     private DcMotor BL;
     private DcMotor FR;
     private DcMotor BR;
+    private Servo claw_servo;
+    private ColorSensor col_sensor;
 
     @Override
     public void runOpMode() {
@@ -29,11 +40,15 @@ public class Auto extends LinearOpMode {
         BL = hardwareMap.dcMotor.get("BL");
         FR = hardwareMap.dcMotor.get("FR");
         BR = hardwareMap.dcMotor.get("BR");
+        claw_servo = hardwareMap.servo.get("claw_servo");
+        col_sensor = hardwareMap.colorSensor.get("color_sensor");
 
         FL.setDirection(DcMotorSimple.Direction.FORWARD);
         BL.setDirection(DcMotorSimple.Direction.FORWARD);
         FR.setDirection(DcMotorSimple.Direction.FORWARD);
         BR.setDirection(DcMotorSimple.Direction.REVERSE);
+        
+        claw_servo.setPosition(SERVO_LOWER);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -43,21 +58,64 @@ public class Auto extends LinearOpMode {
         runtime.reset();
 
         // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
+        // while (opModeIsActive()) {
+        //     mecanumMoveBot(MOVE_SPEED, 0, 0);
+        //     sleep(5000);
+        //     mecanumMoveBot(0, 0, 0);
+
+        //     secondsElapsed = runtime.seconds();
+        //     if (secondsElapsed > RUN_TIME) {
+        //         break;
+        //     }
+
+        //     // Show the elapsed game time and wheel power.
+        //     telemetry.addData("secondsElapsed", secondsElapsed);
+        //     telemetry.update();
+        // }
+        
+        if (opModeIsActive()) {
             mecanumMoveBot(MOVE_SPEED, 0, 0);
-            while (runtime.seconds() < 5) {
-            }
+            waitDistance(15.5);
             mecanumMoveBot(0, 0, 0);
-
-            secondsElapsed = runtime.seconds();
-            if (secondsElapsed > RUN_TIME) {
-                break;
-            }
-
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("secondsElapsed", secondsElapsed);
+            RGBColor coneColor = getRGB();
+            telemetry.addData("Color detected", coneColor);
             telemetry.update();
+            if (coneColor == RGBColor.BLUE) {
+                mecanumMoveBot(MOVE_SPEED, 0, 0);
+                waitDistance(36);
+                mecanumMoveBot(0, 0, 0);
+            }
         }
+        
+        secondsElapsed = runtime.seconds();
+        while (secondsElapsed <= RUN_TIME) {
+            sleep(100);
+        }
+    }
+    
+    private void waitDistance(double inches) {
+        sleep((int) (inches * DIST_TO_TIME));
+    }
+    
+    private RGBColor getRGB() {
+        int red, green, blue;
+        red = col_sensor.red();
+        green = col_sensor.green();
+        blue = col_sensor.blue();
+        
+        if (red >= green && red >= blue) {
+            return RGBColor.RED;
+        }
+        
+        if (green >= red && green >= blue) {
+            return RGBColor.GREEN;
+        }
+        
+        if (blue >= red && blue >= green) {
+            return RGBColor.BLUE;
+        }
+        
+        return RGBColor.INVALID;
     }
 
     /**
@@ -83,3 +141,4 @@ public class Auto extends LinearOpMode {
         BR.setPower(BR_power);
     }
 }
+
