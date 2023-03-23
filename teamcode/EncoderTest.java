536cf2d6b028d2516646a2af4430a49d85a8e205
Yi Liu <yiru.liu05@gmail.com>
2023-03-23 07:35:32 -0400

    Encoder position test

--- teamcode/EncoderTest.java
+++ teamcode/EncoderTest.java
@@ -11,21 +11,29 @@ public class EncoderTest extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        testMotor = hardwareMap.dcMotor.get("motor");
+        testMotor = hardwareMap.dcMotor.get("BR");
 
         testMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        // testMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // testMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         waitForStart();
 
         int encoderPosition;
-        double power;
+        // double power;
+        testMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        testMotor.setPower(1.0);
+        testMotor.setTargetPosition(5000);
+        testMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         while (opModeIsActive()) {
-            power = -gamepad1.left_stick_y;
-            testMotor.setPower(power);
+            // power = -gamepad1.left_stick_y;
+            // testMotor.setPower(power);
             encoderPosition = testMotor.getCurrentPosition();
 
-            telemetry.add("power", power);
-            telemetry.add("position", encoderPosition);
+            // telemetry.addData("power", power);
+            telemetry.addData("position", encoderPosition);
+            telemetry.addData("target position", testMotor.getTargetPosition());
             telemetry.update();
         }
     }
