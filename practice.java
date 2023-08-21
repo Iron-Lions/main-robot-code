d6dbc60be6d05ccf92b89eddeea0b1843b9485a8
Erik Wohlgemuth <alden.wohlgemuth@gmail.com>
2023-08-21 14:29:05 -0400

    add encoder test

--- practice.java
+++ practice.java
@@ -14,13 +14,19 @@ public class ImuTestAlden extends LinearOpMode {
     private IMU imu;
     private double roll;
     private double pitch;
-
+    private double frontRightPower=0;
+    private double frontLeftPower=0;
+    private double backRightPower=0;
+    private double backLeftPower=0;
+    
     @Override
     public void runOpMode() {
         DcMotor motorFrontLeft = hardwareMap.dcMotor.get("Front_Left");
         DcMotor motorBackLeft = hardwareMap.dcMotor.get("Back_Left");
         DcMotor motorFrontRight = hardwareMap.dcMotor.get("Front_Right");
         DcMotor motorBackRight = hardwareMap.dcMotor.get("Back_Right");
+        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         imu = hardwareMap.get(IMU.class, "imu");
         imu.initialize(
             new IMU.Parameters(
@@ -31,35 +37,50 @@ public class ImuTestAlden extends LinearOpMode {
             )
         );
         
+        imu.resetYaw();
+        
         waitForStart();
         while (opModeIsActive()) {
             roll = imu.getRobotYawPitchRollAngles().getRoll(AngleUnit.DEGREES);
             pitch = imu.getRobotYawPitchRollAngles().getPitch(AngleUnit.DEGREES);
-
-            double denominator = 180;
-            //double denominator = Math.max(Math.abs(roll) + Math.abs(pitch), 1);
-
-            /*if (roll>0){
-                motorFrontRight.setPower((roll)/denominator);
-                motorBackRight.setPower((roll)/denominator);
-            }
-            if (roll<0){
-                motorFrontLeft.setPower((roll)/denominator);
-                motorBackLeft.setPower((roll)/denominator);
-            }*/
             
-            if (pitch>0){
-                motorFrontRight.setPower((pitch)/denominator);
-                motorFrontLeft.setPower((pitch)/denominator);
+            double denominator = 180;
+            //double denominator = Math.max(Math.abs(roll) + Math.abs(pitch), 1.0);
+            
+            if (roll > 0){
+                frontRightPower += roll/denominator;
+                backRightPower += roll/denominator;
             }
-            if (pitch<0){
-                motorBackRight.setPower((pitch)/denominator);
-                motorBackLeft.setPower((pitch)/denominator);
+            
+            if (roll < 0){
+                frontLeftPower += roll/denominator;
+                backLeftPower += roll/denominator;
             }
+            
+            if (pitch > 0){
+                frontRightPower += pitch/denominator;
+                frontLeftPower += pitch/denominator;
+            }
+            
+            if (pitch > 0){
+                backRightPower += pitch/denominator;
+                backLeftPower += pitch/denominator;
+            }
+            
+            motorFrontRight.setPower(frontRightPower);
+            motorFrontLeft.setPower(frontLeftPower);
+            motorBackRight.setPower(backRightPower);
+            motorBackLeft.setPower(backLeftPower);
+            
+            frontRightPower = 0;
+            frontLeftPower = 0;
+            backRightPower = 0;
+            backLeftPower = 0;
             
             telemetry.addData("Roll (Degrees)", roll);
             telemetry.addData("Pitch (Degrees)", pitch);
             telemetry.update();
         }
+
     }
 }
