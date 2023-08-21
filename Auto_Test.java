3a41a5368dc82b3fd211444631ebf314b1ae1855
Erik Wohlgemuth <alden.wohlgemuth@gmail.com>
2023-08-21 15:51:57 -0400

    fix auto test

--- Auto_Test.java
+++ Auto_Test.java
@@ -2,19 +2,21 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Autonomous(name = "Auto_Test", group = "")
 
-@Autonomous(name = "moveAuto", group = "")
-
-public class moveAuto extends LinearOpMode {
+public class Auto_Test extends LinearOpMode {
     private IMU imu;
     private double yaw;
+    private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode()  throws InterruptedException{
@@ -33,17 +35,21 @@ public class moveAuto extends LinearOpMode {
         DcMotor motorBackLeft = hardwareMap.dcMotor.get("Back_Left");
         DcMotor motorFrontRight = hardwareMap.dcMotor.get("Front_Right");
         DcMotor motorBackRight = hardwareMap.dcMotor.get("Back_Right");
-
+        
         motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         
         waitForStart();
-        if (isStopRequested()) return;
-
+        runtime.reset();
+        movement('b',1.0,50000);
+        
         while (opModeIsActive()) {
             yaw = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
             
-            
+            motorFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motorFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motorBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motorBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             
             telemetry.addData("yaw", yaw);
             telemetry.update();
@@ -51,23 +57,33 @@ public class moveAuto extends LinearOpMode {
     }
 
     public void movement(char direction, double motorPower, double distance) {
-
+        
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("Front_Left");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("Back_Left");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("Front_Right");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("Back_Right");
+        
+        
+        
         if (direction == 'b'){
             distance = -distance;
         }
 
-        motorFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
         double distanceTravelled = 0;
-        double numOfTicks = (distance/(96*Math.pi))*537.7;
-
-        motorFrontRight.RunMode.RUN_TO_POSITION(distance);
-        motorFrontLeft.RunMode.RUN_TO_POSITION(distance);
-        motorBackRight.RunMode.RUN_TO_POSITION(distance);
-        motorBackRight.RunMode.RUN_TO_POSITION(distance);
-
+        int numOfTicks = (int)Math.round((distance/(96*Math.PI))*537.7);
+        
+        
+        motorFrontRight.setTargetPosition(numOfTicks);
+        motorFrontLeft.setTargetPosition(numOfTicks);
+        motorBackRight.setTargetPosition(numOfTicks);
+        motorBackLeft.setTargetPosition(numOfTicks);
+        
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        
         //distanceTravelled will be calculated with encoders
 
         /*while (distanceTravelled < distance){
@@ -76,12 +92,12 @@ public class moveAuto extends LinearOpMode {
             motorFrontLeft.setPower(motorPower);
             motorBackLeft.setPower(motorPower);
             distanceTravelled = encoder * 96 * 3.14;
-        }*/    
+        } */   
         
-        motorFrontRight.setPower(0);
-        motorBackRight.setPower(0);
-        motorFrontLeft.setPower(0);
-        motorBackLeft.setPower(0);
+        motorFrontRight.setPower(motorPower);
+        motorBackRight.setPower(motorPower);
+        motorFrontLeft.setPower(motorPower);
+        motorBackLeft.setPower(motorPower);
         
     }
 }
