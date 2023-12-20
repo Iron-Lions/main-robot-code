5c4252d82b0307f0eb87be6882b7401e7d535b7c
Jacob Zhang <68299594+JacobZhang0@users.noreply.github.com>
2023-12-20 14:55:15 -0500

    Add Movement

--- TensorFlow.java
+++ TensorFlow.java
@@ -1,119 +1,117 @@
-/* Copyright (c) 2019 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
-import android.util.Size;
-
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import java.util.List;
 
-/*
- * This OpMode illustrates the basics of TensorFlow Object Detection,
- * including Java Builder structures for specifying Vision parameters.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
- */
-@Autonomous(name = "Concept: TensorFlow Object Detection", group = "Concept")
+@Autonomous(name = "TensorFlow", group = "")
 
 public class TensorFlow extends LinearOpMode {
 
-    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+    private DcMotor motorFrontRight;
     private DcMotor motorFrontLeft;
     private DcMotor motorBackLeft;
-    private DcMotor motorFrontRight;
     private DcMotor motorBackRight;
 
+    /*private DcMotor lift;
+    private DcMotor arm;
+    private DcMotor intake;
+    private Servo dumpy_4;
+    private IMU imu;*/
+
     private static final double MOVE_SPEED = 0.5;
+    private static final double MAX_LIFT_POSITION = 0;
+    private static final double MIN_LIFT_POSITION = -3100.0;
+    private static final double MAX_ARM_POSITION = 750.0;
+    private static final double MIN_ARM_POSITION = -10000.0;
+    //Min and Max of dumpy (servo) are scaled between the right most (max) and left most (min) positions
+    private static final double MAX_DUMPY_POSITION = 1.0;
+    private static final double MIN_DUMPY_POSITION = 0.5;
+    private static final double LEFT_LINE = 150.0;
+    private static final double RIGHT_LINE = 500.0;
+    private double x;
+    private double y;
+    private double currentYaw;
+    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+
     // TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
     // this is used when uploading models directly to the RC using the model upload interface.
-    private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/myCustomModel.tflite";
+    private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/TeamPropRed.tflite";
     // Define the labels recognized in the model for TFOD (must be in training order!)
     private static final String[] LABELS = {
-            "Pixel",
+            "Team Prop",
     };
-
-
     private TfodProcessor tfod;
-
     private VisionPortal visionPortal;
 
     @Override
     public void runOpMode() {
+
         motorFrontLeft = hardwareMap.dcMotor.get("Front_Left");
         motorBackLeft = hardwareMap.dcMotor.get("Back_Left");
         motorFrontRight = hardwareMap.dcMotor.get("Front_Right");
         motorBackRight = hardwareMap.dcMotor.get("Back_Right");
+        /*intake = hardwareMap.dcMotor.get("intake");
+        lift = hardwareMap.dcMotor.get("lift");
+        arm = hardwareMap.dcMotor.get("arm");
+        dumpy_4 = hardwareMap.servo.get("dumpy_4");
 
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);*/
+        
         motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         initTfod();
-
-        // Wait for the DS start button to be touched.
-        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
-        telemetry.addData(">", "Touch Play to start OpMode");
-        telemetry.update();
         waitForStart();
 
         if (opModeIsActive()) {
-            while (opModeIsActive()) {
+            
+            
+            telemetryTfod();
 
-                telemetryTfod();
-
-                // Push telemetry to the Driver Station.
-                telemetry.update();
-
-                // Save CPU resources; can resume streaming when needed.
-                if (gamepad1.dpad_down) {
-                    visionPortal.stopStreaming();
-                } else if (gamepad1.dpad_up) {
-                    visionPortal.resumeStreaming();
-                }
-
-                // Share the CPU.
-                sleep(20);
+            if (x < LEFT_LINE) {
+                mecanumMoveBot(0, 0.5, 0);
+                sleep(500);
+                mecanumMoveBot(0.5, 0, 0);
+                sleep(750);
+                mecanumMoveBot(0, 0, 0);
             }
+            if (x > LEFT_LINE && x < RIGHT_LINE) {
+                mecanumMoveBot(0.5, 0, 0);
+                sleep(750);
+                mecanumMoveBot(0, 0, 0);
+            }
+            if (x > RIGHT_LINE) {
+                mecanumMoveBot(0, -0.5, 0);
+                sleep(500);
+                mecanumMoveBot(0.5, 0, 0);
+                sleep(750);
+                mecanumMoveBot(0, 0, 0);
+            }
+            
+
         }
 
-        // Save more CPU resources when camera is no longer needed.
+        // Save CPU resources when camera is no longer needed.
         visionPortal.close();
 
     }
@@ -129,11 +127,11 @@ public class TensorFlow extends LinearOpMode {
                 //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
                 //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
                 //.setModelAssetName(TFOD_MODEL_ASSET)
-                //.setModelFileName(TFOD_MODEL_FILE)
+                .setModelFileName(TFOD_MODEL_FILE)
 
                 // The following default settings are available to un-comment and edit as needed to
                 // set parameters for custom models.
-                //.setModelLabels(LABELS)
+                .setModelLabels(LABELS)
                 //.setIsModelTensorFlow2(true)
                 //.setIsModelQuantized(true)
                 //.setModelInputSize(300)
@@ -152,8 +150,7 @@ public class TensorFlow extends LinearOpMode {
         }
 
         // Choose a camera resolution. Not all cameras support all resolutions.
-        //builder.setCameraResolution(new Size(640, 480));
-        builder.setCameraResolution(new Size(1280, 720));
+        //builder.setCameraResolution(new Size(1280, 720));
 
         // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
         builder.enableLiveView(true);
@@ -173,7 +170,7 @@ public class TensorFlow extends LinearOpMode {
         visionPortal = builder.build();
 
         // Set confidence threshold for TFOD recognitions, at any time.
-        tfod.setMinResultConfidence(0.75f);
+        tfod.setMinResultConfidence(0.5f);
 
         // Disable or re-enable the TFOD processor at any time.
         //visionPortal.setProcessorEnabled(tfod, true);
@@ -187,35 +184,73 @@ public class TensorFlow extends LinearOpMode {
 
         // Step through the list of recognitions and display info for each one.
         for (Recognition recognition : currentRecognitions) {
-            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
+            x = (recognition.getLeft() + recognition.getRight()) / 2 ;
             double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
 
             telemetry.addData(""," ");
             telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
             telemetry.addData("- Position", "%.0f / %.0f", x, y);
             telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
+            telemetry.update();
         }
 
     }
 
     private void mecanumMoveBot(double FB_translation, double LR_translation, double rotation) {
-        double FL_power = FB_translation;
-        double BL_power = FB_translation;
-        double FR_power = FB_translation;
-        double BR_power = FB_translation;
-        FL_power += LR_translation;
-        FR_power -= LR_translation;
-        BL_power -= LR_translation;
-        BR_power += LR_translation;
-        FL_power += rotation;
-        FR_power -= rotation;
-        BL_power += rotation;
-        BR_power -= rotation;
+        double FL_power = FB_translation + LR_translation + rotation;
+        double BL_power = FB_translation - LR_translation + rotation;
+        double FR_power = FB_translation - LR_translation - rotation;
+        double BR_power = FB_translation + LR_translation - rotation;
 
         motorFrontLeft.setPower(FL_power);
         motorBackLeft.setPower(BL_power);
         motorFrontRight.setPower(FR_power);
         motorBackRight.setPower(BR_power);
     }
+
+    /*private void armMovement(double power, int targetPosition) {
+        if (targetPosition >= MAX_ARM_POSITION) {
+            arm.setTargetPosition((int)MAX_ARM_POSITION);
+            arm.setPower(power);
+        }
+        if (targetPosition <= MIN_ARM_POSITION) {
+            arm.setTargetPosition((int) MIN_ARM_POSITION);
+            arm.setPower(power);
+        }
+        else {
+            arm.setTargetPosition(targetPosition);
+            arm.setPower(power);
+        }
+    }
+
+    private void liftMovement(double power, int targetPosition) {
+        if (targetPosition >= MAX_LIFT_POSITION) {
+            lift.setTargetPosition((int) MAX_LIFT_POSITION);
+            lift.setPower(power);
+        }
+        if (targetPosition <= MIN_LIFT_POSITION) {
+            lift.setTargetPosition((int) MIN_LIFT_POSITION);
+            lift.setPower(power);
+        }
+        else {
+            lift.setTargetPosition(targetPosition);
+            lift.setPower(power);
+        }
+    }
+
+    private void intakeMovement(double power, double setTime) { //setTime is in ms
+        intake.setPower(power);
+        sleep((long) setTime);
+        intake.setPower(0);
+    }
+
+    private void dumpy_4Movement(double targetPosition) {
+        if (targetPosition >= MAX_DUMPY_POSITION) {
+            dumpy_4.setPosition(MAX_DUMPY_POSITION);
+        }
+        else {
+            dumpy_4.setPosition(Math.max(targetPosition, MIN_DUMPY_POSITION));
+        }
+    }*/
 
 }
