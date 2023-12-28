3d99af4a74571cb234b3ef0785508eb799d9a0cc
Jacob Zhang <68299594+JacobZhang0@users.noreply.github.com>
2023-12-27 22:04:02 -0500

    Bug Fixes

--- Autonomous2023_24.java
+++ Autonomous2023_24.java
@@ -18,7 +18,6 @@ import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 import java.util.List;
 
 @Autonomous(name = "Autonomous2023_24", group = "")
-
 public class Autonomous2023_24 extends LinearOpMode {
 
     private DcMotor motorFrontRight;
@@ -34,34 +33,27 @@ public class Autonomous2023_24 extends LinearOpMode {
     private static final double MIN_LIFT_POSITION = -3100.0;
     private static final double MAX_ARM_POSITION = 750.0;
     private static final double MIN_ARM_POSITION = -10000.0;
-    //Min and Max of dumpy (servo) are scaled between the right most (max) and left most (min) positions
     private static final double MAX_DUMPY_POSITION = 1.0;
     private static final double MIN_DUMPY_POSITION = 0.5;
     private static final double LEFT_LINE = 150.0;
     private static final double RIGHT_LINE = 300.0;
     private double x;
     private int objectNum = 0;
-    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+    private static final boolean USE_WEBCAM = true;
     private WebcamName webcam1;
-
-    // TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
-    // this is used when uploading models directly to the RC using the model upload interface.
     private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/TeamPropRed.tflite";
-    // Define the labels recognized in the model for TFOD (must be in training order!)
     private static final String[] LABELS = {
             "Team Prop",
     };
     private TfodProcessor tfod;
     private VisionPortal visionPortal;
-
-    //April Tag
     private static final double DESIRED_DISTANCE = 12.0;
     private static final double APRIL_TAG_SPEED_GAIN = 0.02;
     private static final double APRIL_TAG_STRAFE_GAIN = 0.015;
     private static final double APRIL_TAG_TURN_GAIN = 0.01;
     private static final double APRIL_TAG_MAX_AUTO_SPEED = 0.5;
-    private static final double APRIL_TAG_MAX_AUTO_STRAFE= 0.5;
-    private static final double APRIL_TAG_MAX_AUTO_TURN  = 0.3;
+    private static final double APRIL_TAG_MAX_AUTO_STRAFE = 0.5;
+    private static final double APRIL_TAG_MAX_AUTO_TURN = 0.3;
     private static int desiredTagId = -1;
     private AprilTagProcessor aprilTag;
     private AprilTagDetection desiredTag = null;
@@ -70,11 +62,12 @@ public class Autonomous2023_24 extends LinearOpMode {
     private double drive = 0;
     private double strafe = 0;
     private double turn = 0;
-
+    double rangeError = -1;
+    private double headingError = -1;
+    private double yawError = -1;
 
     @Override
     public void runOpMode() {
-
         motorFrontLeft = hardwareMap.dcMotor.get("Front_Left");
         motorBackLeft = hardwareMap.dcMotor.get("Back_Left");
         motorFrontRight = hardwareMap.dcMotor.get("Front_Right");
@@ -101,11 +94,9 @@ public class Autonomous2023_24 extends LinearOpMode {
         waitForStart();
 
         if (opModeIsActive()) {
-
             while (opModeIsActive() && objectNum == 0) {
                 telemetryTfod();
             }
-
 
             if (x < LEFT_LINE) {
                 desiredTagId = 4;
@@ -114,113 +105,62 @@ public class Autonomous2023_24 extends LinearOpMode {
                 mecanumMoveBot(MOVE_SPEED, 0, 0);
                 sleep(750);
                 mecanumMoveBot(0, 0, 0);
-                // Still Need to Add movement to face AprilTags
-                while (opModeIsActive() && desiredTagId != detectedTagId) {
+                while (opModeIsActive() && rangeError != 0 && headingError != 0 && yawError != 0) {
                     aprilTagDetectionMovement();
                 }
-            }
-            if (x > LEFT_LINE && x < RIGHT_LINE) {
+            } else if (x > LEFT_LINE && x < RIGHT_LINE) {
                 desiredTagId = 5;
                 mecanumMoveBot(MOVE_SPEED, 0, 0);
                 sleep(1000);
                 mecanumMoveBot(0, 0, 0);
-                // Still Need to Add movement to face AprilTags
-                while (opModeIsActive() && desiredTagId != detectedTagId) {
+                while (opModeIsActive() && rangeError != 0 && headingError != 0 && yawError != 0) {
                     aprilTagDetectionMovement();
                 }
-            }
-            if (x > RIGHT_LINE) {
+            } else if (x > RIGHT_LINE) {
                 desiredTagId = 6;
                 mecanumMoveBot(0, -MOVE_SPEED, 0);
                 sleep(500);
                 mecanumMoveBot(MOVE_SPEED, 0, 0);
                 sleep(750);
                 mecanumMoveBot(0, 0, 0);
-                // Still Need to Add movement to face AprilTags
-                while (opModeIsActive() && desiredTagId != detectedTagId) {
+                while (opModeIsActive() && rangeError != 0 && headingError != 0 && yawError != 0) {
                     aprilTagDetectionMovement();
                 }
             }
-
-
         }
 
-        // Save CPU resources when camera is no longer needed.
         visionPortal.close();
-
     }
 
     private void initTfod() {
-
-        // Create the TensorFlow processor by using a builder.
         tfod = new TfodProcessor.Builder()
-
-                // With the following lines commented out, the default TfodProcessor Builder
-                // will load the default model for the season. To define a custom model to load,
-                // choose one of the following:
-                //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
-                //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
-                //.setModelAssetName(TFOD_MODEL_ASSET)
                 .setModelFileName(TFOD_MODEL_FILE)
-
-                // The following default settings are available to un-comment and edit as needed to
-                // set parameters for custom models.
                 .setModelLabels(LABELS)
-                //.setIsModelTensorFlow2(true)
-                //.setIsModelQuantized(true)
-                //.setModelInputSize(300)
-                //.setModelAspectRatio(16.0 / 9.0)
-
                 .build();
 
-        // Create the vision portal by using a builder.
         VisionPortal.Builder builder = new VisionPortal.Builder();
 
-        // Set the camera (webcam vs. built-in RC phone camera).
         if (USE_WEBCAM) {
             builder.setCamera(webcam1);
         } else {
             builder.setCamera(BuiltinCameraDirection.BACK);
         }
 
-        // Choose a camera resolution. Not all cameras support all resolutions.
-        //builder.setCameraResolution(new Size(1280, 720));
-
-        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
         builder.enableLiveView(true);
-
-        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
-        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
-
-        // Choose whether or not LiveView stops if no processors are enabled.
-        // If set "true", monitor shows solid orange screen if no processors enabled.
-        // If set "false", monitor shows camera view without annotations.
-        //builder.setAutoStopLiveView(false);
-
-        // Set and enable the processor.
         builder.addProcessor(tfod);
 
-        // Build the Vision Portal, using the above settings.
         visionPortal = builder.build();
-
-        // Set confidence threshold for TFOD recognitions, at any time.
         tfod.setMinResultConfidence(0.5f);
-
-        // Disable or re-enable the TFOD processor at any time.
-        //visionPortal.setProcessorEnabled(tfod, true);
-
     }
 
     private void telemetryTfod() {
-
         List<Recognition> currentRecognitions = tfod.getRecognitions();
         objectNum = currentRecognitions.size();
         telemetry.addData("# Objects Detected", objectNum);
 
-        // Step through the list of recognitions and display info for each one.
         for (Recognition recognition : currentRecognitions) {
-            x = (recognition.getLeft() + recognition.getRight()) / 2 ;
-            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+            x = (recognition.getLeft() + recognition.getRight()) / 2;
+            double y = (recognition.getTop() + recognition.getBottom()) / 2;
 
             telemetry.addData(""," ");
             telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
@@ -228,23 +168,12 @@ public class Autonomous2023_24 extends LinearOpMode {
             telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
             telemetry.update();
         }
-
     }
 
     private void initAprilTag() {
-        // Create the AprilTag processor by using a builder.
         aprilTag = new AprilTagProcessor.Builder().build();
-
-        // Adjust Image Decimation to trade-off detection-range for detection-rate.
-        // eg: Some typical detection data using a Logitech C920 WebCam
-        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
-        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
-        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second
-        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second
-        // Note: Decimation can be changed on-the-fly to adapt during a match.
         aprilTag.setDecimation(2);
 
-        // Create the vision portal by using a builder.
         if (USE_WEBCAM) {
             visionPortal = new VisionPortal.Builder()
                     .setCamera(webcam1)
@@ -259,54 +188,42 @@ public class Autonomous2023_24 extends LinearOpMode {
     }
 
     private void aprilTagDetectionMovement() {
-        // Step through the list of detected tags and look for a matching tag
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
         for (AprilTagDetection detection : currentDetections) {
-            // Look to see if we have size info on this tag.
             if (detection.metadata != null) {
-                //  Check to see if we want to track towards this tag.
                 if ((desiredTagId < 0) || (detection.id == desiredTagId)) {
-                    // Yes, we want to use this tag.
                     targetFound = true;
                     desiredTag = detection;
-                    break;  // don't look any further.
+                    break;
                 } else {
-                    // This tag is in the library, but we do not want to track it right now.
                     telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
                 }
             } else {
-                // This tag is NOT in the library, so we don't have enough information to track to it.
                 telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
             }
             detectedTagId = detection.id;
         }
 
-        // Tell the driver what we see, and what to do.
         if (targetFound) {
-            telemetry.addData("\n>","HOLD Left-Bumper to Drive to Target\n");
             telemetry.addData("Found", "ID %d (%s)", desiredTag.id, desiredTag.metadata.name);
             telemetry.addData("Range",  "%5.1f inches", desiredTag.ftcPose.range);
             telemetry.addData("Bearing","%3.0f degrees", desiredTag.ftcPose.bearing);
             telemetry.addData("Yaw","%3.0f degrees", desiredTag.ftcPose.yaw);
             sleep(1000);
 
-            // Determine heading, range and Yaw (tag image rotation) error so we can use them to control the robot automatically.
-            double  rangeError      = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
-            double  headingError    = desiredTag.ftcPose.bearing;
-            double  yawError        = desiredTag.ftcPose.yaw;
+            double rangeError = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
+            double headingError = desiredTag.ftcPose.bearing;
+            double yawError = desiredTag.ftcPose.yaw;
 
-            // Use the speed and turn "gains" to calculate how we want the robot to move.
-            drive  = Range.clip(rangeError * APRIL_TAG_SPEED_GAIN, -APRIL_TAG_MAX_AUTO_SPEED, APRIL_TAG_MAX_AUTO_SPEED);
-            turn   = Range.clip(headingError * APRIL_TAG_TURN_GAIN, -APRIL_TAG_MAX_AUTO_TURN, APRIL_TAG_MAX_AUTO_TURN) ;
+            drive = Range.clip(rangeError * APRIL_TAG_SPEED_GAIN, -APRIL_TAG_MAX_AUTO_SPEED, APRIL_TAG_MAX_AUTO_SPEED);
+            turn = Range.clip(headingError * APRIL_TAG_TURN_GAIN, -APRIL_TAG_MAX_AUTO_TURN, APRIL_TAG_MAX_AUTO_TURN) ;
             strafe = Range.clip(-yawError * APRIL_TAG_STRAFE_GAIN, -APRIL_TAG_MAX_AUTO_STRAFE, APRIL_TAG_MAX_AUTO_STRAFE);
 
             telemetry.addData("Auto","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
         }
         telemetry.update();
 
-        // Apply desired axes motions to the drivetrain.
-        aprilTagMoveRobot(drive, strafe, turn);
-        sleep(10);
+        aprilTagMoveBot(drive, strafe, turn);
     }
 
     private void mecanumMoveBot(double FB_translation, double LR_translation, double rotation) {
@@ -320,14 +237,13 @@ public class Autonomous2023_24 extends LinearOpMode {
         motorFrontRight.setPower(FR_power);
         motorBackRight.setPower(BR_power);
     }
-    public void aprilTagMoveRobot(double x, double y, double yaw) {
-        // Calculate wheel powers.
-        double frontLeftPower    =  x -y -yaw;
-        double frontRightPower   =  x +y +yaw;
-        double backLeftPower     =  x +y -yaw;
-        double backRightPower    =  x -y +yaw;
 
-        // Normalize wheel powers to be less than 1.0
+    public void aprilTagMoveBot(double x, double y, double yaw) {
+        double frontLeftPower =  x - y - yaw;
+        double frontRightPower =  x + y + yaw;
+        double backLeftPower =  x + y - yaw;
+        double backRightPower =  x - y + yaw;
+
         double max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
         max = Math.max(max, Math.abs(backLeftPower));
         max = Math.max(max, Math.abs(backRightPower));
@@ -339,7 +255,6 @@ public class Autonomous2023_24 extends LinearOpMode {
             backRightPower /= max;
         }
 
-        // Send powers to the wheels.
         motorFrontLeft.setPower(frontLeftPower);
         motorFrontRight.setPower(frontRightPower);
         motorBackLeft.setPower(backLeftPower);
@@ -354,8 +269,7 @@ public class Autonomous2023_24 extends LinearOpMode {
         if (targetPosition <= MIN_ARM_POSITION) {
             arm.setTargetPosition((int) MIN_ARM_POSITION);
             arm.setPower(power);
-        }
-        else {
+        } else {
             arm.setTargetPosition(targetPosition);
             arm.setPower(power);
         }
@@ -369,14 +283,13 @@ public class Autonomous2023_24 extends LinearOpMode {
         if (targetPosition <= MIN_LIFT_POSITION) {
             lift.setTargetPosition((int) MIN_LIFT_POSITION);
             lift.setPower(power);
-        }
-        else {
+        } else {
             lift.setTargetPosition(targetPosition);
             lift.setPower(power);
         }
     }
 
-    private void intakeMovement(double power, double setTime) { //setTime is in ms
+    private void intakeMovement(double power, double setTime) {
         intake.setPower(power);
         sleep((long) setTime);
         intake.setPower(0);
@@ -385,10 +298,8 @@ public class Autonomous2023_24 extends LinearOpMode {
     private void dumpy_4Movement(double targetPosition) {
         if (targetPosition >= MAX_DUMPY_POSITION) {
             dumpy_4.setPosition(MAX_DUMPY_POSITION);
-        }
-        else {
+        } else {
             dumpy_4.setPosition(Math.max(targetPosition, MIN_DUMPY_POSITION));
         }
     }
-
 }
