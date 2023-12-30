88c4f7f5b7e8febe5b0782d9539e8a047e7cac54
Jacob Zhang <jacob.zhang8@icloud.com>
2023-12-30 17:59:14 -0500

    Add Comments & Movement(Needs Tuning)

--- TensorFlow.java
+++ TensorFlow.java
@@ -86,93 +86,55 @@ public class TensorFlow extends LinearOpMode {
 
 
             if (x < LEFT_LINE) {
-                mecanumMoveBot(0, MOVE_SPEED, 0);
-                sleep(500);
                 mecanumMoveBot(MOVE_SPEED, 0, 0);
-                sleep(750);
+                sleep(1200); // Tuning
+                mecanumMoveBot(0, 0, MOVE_SPEED);
+                sleep(940); // Tuning
                 mecanumMoveBot(0, 0, 0);
-                
-            }
-            else if (x > LEFT_LINE && x < RIGHT_LINE) {
-                mecanumMoveBot(MOVE_SPEED, 0, 0);
-                sleep(1000);
-                mecanumMoveBot(0, 0, 0);
-            }
-            else if (x > RIGHT_LINE) {
-                mecanumMoveBot(0, -MOVE_SPEED, 0);
-                sleep(500);
-                mecanumMoveBot(MOVE_SPEED, 0, 0);
-                sleep(750);
-                mecanumMoveBot(0, 0, 0);
-            }
+                // Drop Pixel
 
+            }
+            if (x > LEFT_LINE && x < RIGHT_LINE) {
+                mecanumMoveBot(MOVE_SPEED, 0, 0);
+                sleep(1200); // Tuning
+                mecanumMoveBot(0, 0, 0);
+                //Drop pixel
+            }
+            if (x > RIGHT_LINE) {
+                mecanumMoveBot(MOVE_SPEED, 0, 0);
+                sleep(1200); // Tuning
+                mecanumMoveBot(0, 0, -MOVE_SPEED);
+                sleep(940); // Tuning
+                mecanumMoveBot(0, 0, 0);
+                // Drop pixel
+            }
 
         }
-
-        // Save CPU resources when camera is no longer needed.
         visionPortal.close();
 
     }
 
     private void initTfod() {
 
-        // Create the TensorFlow processor by using a builder.
         tfod = new TfodProcessor.Builder()
 
-                // With the following lines commented out, the default TfodProcessor Builder
-                // will load the default model for the season. To define a custom model to load,
-                // choose one of the following:
-                //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
-                //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
-                //.setModelAssetName(TFOD_MODEL_ASSET)
                 .setModelFileName(TFOD_MODEL_FILE)
-
-                // The following default settings are available to un-comment and edit as needed to
-                // set parameters for custom models.
                 .setModelLabels(LABELS)
-                //.setIsModelTensorFlow2(true)
-                //.setIsModelQuantized(true)
-                //.setModelInputSize(300)
-                //.setModelAspectRatio(16.0 / 9.0)
-
                 .build();
 
-        // Create the vision portal by using a builder.
         VisionPortal.Builder builder = new VisionPortal.Builder();
 
-        // Set the camera (webcam vs. built-in RC phone camera).
         if (USE_WEBCAM) {
             builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
         } else {
             builder.setCamera(BuiltinCameraDirection.BACK);
         }
 
-        // Choose a camera resolution. Not all cameras support all resolutions.
-        //builder.setCameraResolution(new Size(1280, 720));
-
-        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
         builder.enableLiveView(true);
-
-        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
-        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
-
-        // Choose whether or not LiveView stops if no processors are enabled.
-        // If set "true", monitor shows solid orange screen if no processors enabled.
-        // If set "false", monitor shows camera view without annotations.
-        //builder.setAutoStopLiveView(false);
-
-        // Set and enable the processor.
         builder.addProcessor(tfod);
 
-        // Build the Vision Portal, using the above settings.
         visionPortal = builder.build();
-
-        // Set confidence threshold for TFOD recognitions, at any time.
         tfod.setMinResultConfidence(0.5f);
-
-        // Disable or re-enable the TFOD processor at any time.
-        //visionPortal.setProcessorEnabled(tfod, true);
-
     }
 
     private void telemetryTfod() {
